f42cbe6d168bde7759471781ac23131737330479
Angela Alves <angelavbalves@rede.ulbra.br>
2022-07-07 15:33:26 -0300

    Validação do formulárioe novo setor de dicas e resenhas

--- IDW/hedone/assets/js/script.js
+++ IDW/hedone/assets/js/script.js
@@ -75,5 +75,42 @@ var x = setInterval(function() {
     clearInterval(x);
     document.getElementById("dia").innerHTML = "Promoção encerrada";
   }
+
 }, 1000);
+
+function verificar() {
+    var nome = document.getElementById("nome").value;
+    var emailCliente = document.getElementById("meuemail").value;
+    var mensagem = document.getElementById("mensagem").value;
+    if(nome == "" || nome.length < 3) {
+        alert("Nome não informado");
+    } 
+    if (emailCliente == '' || !isEmail(emailCliente)) {
+        alert('Email inválido, digite novamente');
+    }
+    if (mensagem == "" || mensagem.length < 3) {
+        alert("Você não digitou uma mensagem");
+    }
+    if (nome != "" && nome.length > 3 && 
+        emailCliente != '' && isEmail(emailCliente) && 
+        mensagem != "" && mensagem.length > 3 ) {
+            abrirModal()
+    }
+}
+
+function abrirModal() {
+    let modal = document.querySelector('.modal');
+    modal.style.display = 'block';
+    console.log("Testando funcao")
+}
+
+function esconderModal() {
+    let modal = document.querySelector('.modal');
+    modal.style.display = 'none';
+}
+
+function isEmail(email) {
+    return /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(email)
+}
+
 
